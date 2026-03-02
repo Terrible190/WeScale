3cc6b5282b9bef9556624c384d07b1c0586cb577
Guillem Munne <guillemmunnemarchena@gmail.com>
2026-03-02 21:10:21 +0100

    Afegir la estructura basica per provar el pomelo

---
+++ Project/WeScale/WeScale/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeScale.ViewModels
+{
+    internal class MainViewModel
+    {
+        
+        public MainViewModel()
+        {
+
+
+        }
+    }
+}
