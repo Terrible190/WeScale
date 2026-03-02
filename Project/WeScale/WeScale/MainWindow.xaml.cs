8d66f6841bc365f7421357d24bbcceddf859aaa7
Guillem Munne <guillemmunnemarchena@gmail.com>
2026-03-02 21:15:35 +0100

    Canvis de estructura basica

--- Project/WeScale/WeScale/MainWindow.xaml.cs
+++ Project/WeScale/WeScale/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WeScale.ViewModels;
 
 namespace WeScale
 {
@@ -23,6 +24,7 @@ namespace WeScale
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
