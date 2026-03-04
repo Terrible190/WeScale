5034c080d0a1b5f3d86333cb54232d1ade38bce9
Guillem Munne <guillemmunnemarchena@gmail.com>
2026-03-04 20:19:03 +0100

    Afegir codic al mainviewmodel

--- Project/WeScale/WeScale/ViewModels/MainViewModel.cs
+++ Project/WeScale/WeScale/ViewModels/MainViewModel.cs
@@ -2,18 +2,46 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using WeScale.UserContr;
+using BD.Models;
 
 namespace WeScale.ViewModels
 {
-    internal class MainViewModel
+
+    private readonly AppDbContext _context;
+
+    public ObservableCollection<Personatge> Personatges { get; set; }
+
+    public ObservableCollection<Accio> Armes { get; set; }
+    public ObservableCollection<Accio> Habilitats { get; set; }
+    public ObservableCollection<Accio> Objectes { get; set; }
+
+    public MainViewModel()
     {
-        public ObservableCollection<UsCo_CartaGeneric> CartasGenericas { get; set; }
-        public MainViewModel()
-        {
-            
-        }
+        _context = new AppDbContext();
+
+        // Personatges
+        Personatges = new ObservableCollection<Personatge>(
+            _context.Personatges.ToList()
+        );
+
+        // Accions
+        var accions = _context.Accions.ToList();
+
+        Armes = new ObservableCollection<Accio>(
+            accions.Where(a => a.Tipus == 1)
+        );
+
+        Habilitats = new ObservableCollection<Accio>(
+            accions.Where(a => a.Tipus == 2)
+        );
+
+        Objectes = new ObservableCollection<Accio>(
+            accions.Where(a => a.Tipus == 3)
+        );
     }
 }
