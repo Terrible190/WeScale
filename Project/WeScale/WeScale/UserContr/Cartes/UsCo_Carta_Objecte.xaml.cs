7396ee0f28cba1c0ba7f697995dd0821f6b3fd44
rredondo <rredondoaguilera@gmail.com>
2026-03-09 19:55:29 +0100

    sha afegit user controls de cartes

---
+++ Project/WeScale/WeScale/UserContr/Cartes/UsCo_Carta_Objecte.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeScale.UserContr.Cartes
+{
+    /// <summary>
+    /// Interaction logic for UsCo_Carta_Objecte.xaml
+    /// </summary>
+    public partial class UsCo_Carta_Objecte : UserControl
+    {
+        public UsCo_Carta_Objecte()
+        {
+            InitializeComponent();
+        }
+    }
+}
