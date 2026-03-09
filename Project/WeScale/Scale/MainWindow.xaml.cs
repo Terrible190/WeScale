83e78a7bc59eb846ed5983cd35210c0580ddf049
rredondo <rredondoaguilera@gmail.com>
2026-03-09 20:19:51 +0100

    canvi WeScale net framwork a net

--- Project/WeScale/Scale/MainWindow.xaml.cs
+++ Project/WeScale/Scale/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace WeScale
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
+           DataContext = new MainViewModel();
         }
     }
 }
