91256dbd328505d8a5acca2126f73de6a67acaa0
rredondo <rredondoaguilera@gmail.com>
2026-03-11 20:16:26 +0100

    funcionament de filtres llista i cerca per nom i id

---
+++ Project/WeScale/Scale/CartaTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BD.Models;
+
+namespace WeScale
+{
+    public class CartaTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate PersonatgeTemplate { get; set; }
+        public DataTemplate ArmaTemplate { get; set; }
+        public DataTemplate HabilitatTemplate { get; set; }
+        public DataTemplate ObjecteTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Personatge)
+                return PersonatgeTemplate;
+
+            if (item is Accio accio)
+            {
+                if (accio.Tipus == 1)
+                    return ArmaTemplate;
+
+                if (accio.Tipus == 2)
+                    return HabilitatTemplate;
+
+                if (accio.Tipus == 3)
+                    return ObjecteTemplate;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
---
+++ Project/WeScale/Scale/Model/Carta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeScale.Model
+{
+    public class Carta
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Tipus { get; set; }
+
+        public string Imatge { get; set; }
+
+        public object Dades { get; set; }
+    }
+}
