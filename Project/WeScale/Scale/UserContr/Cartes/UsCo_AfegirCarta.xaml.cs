e320fc525eaa8ab346ca73ff95d98cd35ff37371
Usuari <anasyaagoubi46@gmail.com>
2026-03-13 19:04:24 +0100

    Pulir afegircarta.xaml

--- Project/WeScale/Scale/UserContr/Cartes/UsCo_AfegirCarta.xaml.cs
+++ Project/WeScale/Scale/UserContr/Cartes/UsCo_AfegirCarta.xaml.cs
@@ -18,11 +18,58 @@ namespace WeScale.UserContr
     /// <summary>
     /// Interaction logic for AfegirCarta.xaml
     /// </summary>
-    public partial class AfegirCarta : UserControl
+    public partial class UsCo_AfegirCarta : UserControl
     {
-        public AfegirCarta()
+        public UsCo_AfegirCarta()
         {
             InitializeComponent();
+
+
+            string[] opciones = { "Personatge", "Arma", "Habilitat", "Item" };
+            foreach (var opcion in opciones)
+            {
+                Tipus.Items.Add(opcion);
+            }
+
+            if (Tipus.Items.Count > 0)
+            {
+                Tipus.SelectedIndex = 1;
+            }
+
+
+        }
+
+        private void Tipus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = sender as ComboBox;
+
+            if (combo.SelectedItem != null)
+            {
+                string valor = combo.SelectedItem.ToString();
+
+                switch (valor)
+                {
+                    case "Personatge":
+                        ContentArea.Content = new SubUsCo_AfegirCarta.AfegirPersonatge();
+                        break;
+
+                    case "Arma":
+                        ContentArea.Content = null;
+                        break;
+
+                    case "Habilitat":
+                        ContentArea.Content = null;
+                        break;
+
+                    case "Item":
+                        ContentArea.Content = null;
+                        break;
+
+                    default:
+                        ContentArea.Content = null;
+                        break;
+                }
+            }
         }
     }
 }
